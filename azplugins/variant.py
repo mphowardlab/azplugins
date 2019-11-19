6d0052837e737771df6cab968f48647039621c37
Michael Howard <mphoward@utexas.edu>
2019-11-19 11:58:32 -0600

    Cleanup documentation

--- azplugins/variant.py
+++ azplugins/variant.py
@@ -10,31 +10,34 @@ from . import _azplugins
 class sphere_area(hoomd.variant._variant):
     r"""Radius of sphere contracting with a constant rate of surface-area reduction.
 
-    The volume of the sphere is reduced according to:
+    Args:
+        R0 (float): Initial radius (units: length).
+        alpha (float): Rate of surface-area reduction (units: area per timestep).
+
+    The radius of the sphere :math:`R(t)` is reduced according to:
     .. math::
 
         R(t) = \sqrt{R(0)^2 - (\alpha/4\pi) t}
 
-    where :math:`\alpha` is the rate of surface-area reduction per time.
-    This physics corresponds to the evaporation of a droplet into stagnant air
-    under the mapping:
+    where :math:`R(0)` is the initial radius and :math:`\alpha` is the rate of
+    surface-area reduction per time. This physics corresponds to the evaporation
+    of a droplet into stagnant air under the mapping:
     .. math::
 
         \alpha = \frac{8 \pi D_{\rm v} m \Delta p}{\rho_{\rm l} k_{\rm B} T}
 
-    with :math:`D_{\rm v}` the diffusivity in the vapor, *m* the mass of the
+    with :math:`D_{\rm v}` the diffusivity in the air, *m* the mass of the
     evaporating solvent molecule, :math:`\Delta p` the difference in vapor
-    pressure between the droplet and far away, and :math:`\rho_{\rm l}` the
-    mass density of the liquid.
+    pressure between the droplet surface and far away, and :math:`\rho_{\rm l}`
+    is the mass density of the liquid.
 
-    To be physical, :math:`R(t)`` is never negative. It is fixed to 0 if *t*
-    would make the square-root argument negative.
-
-    Setting \f$\alpha < 0\f$ will cause the sphere to expand.
+    To be physical, :math:`R(t)` is never negative. It is fixed to 0 if *t*
+    would make the square-root argument negative. Setting :math:`\alpha < 0`
+    will cause the sphere to instead expand.
 
     Example::
 
-        variant.sphere(R0=50., alpha=1.)
+        variant.sphere_area(R0=50., alpha=1.)
 
     """
     def __init__(self, R0, alpha):
