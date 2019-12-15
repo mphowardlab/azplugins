ec662c851c405f0c7bbf514d7293a38a6676f3db
Michael Howard <mphoward@utexas.edu>
2019-12-15 17:46:28 -0600

    Add constant flow field (#21)

---
+++ azplugins/test-py/test_flow_constant.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2018-2019, Michael P. Howard
+# This file is part of the azplugins project, released under the Modified BSD License.
+
+# Maintainer: mphoward
+
+from hoomd import *
+context.initialize()
+try:
+    from hoomd import azplugins
+except ImportError:
+    import azplugins
+import unittest
+import numpy as np
+
+# azplugins.flow.constant
+class flow_constant_tests(unittest.TestCase):
+    def test(self):
+        u = azplugins.flow.constant(U=(1,-2,3))
+
+        # try one spot
+        vel = u([-2.,1.,-4.])
+        np.testing.assert_array_almost_equal(vel, (1,-2,3))
+
+        # try a different spot
+        vel = u((2.,-1.,4.))
+        np.testing.assert_array_almost_equal(vel, (1,-2,3))
+
+if __name__ == '__main__':
+    unittest.main(argv = ['test.py', '-v'])
