bf08c617eaeb60f4eadde1ad759c79133f2ace90
Antonia Statt <statt@illinois.edu>
2020-02-17 13:38:39 -0600

    Added EternalField.cu content to MPCDStreamingMethod.cu

--- azplugins/MPCDStreamingMethod.cu
+++ azplugins/MPCDStreamingMethod.cu
@@ -3,6 +3,14 @@
 
 // Maintainer: astatt
 
+#include "ExternalField.h"
+#include "hoomd/GPUPolymorph.cuh"
+
+template mpcd::BlockForce* hoomd::gpu::device_new(Scalar,Scalar,Scalar);
+template mpcd::ConstantForce* hoomd::gpu::device_new(Scalar3);
+template mpcd::SineForce* hoomd::gpu::device_new(Scalar,Scalar);
+template void hoomd::gpu::device_delete(mpcd::ExternalField*);
+
 #include "hoomd/mpcd/ConfinedStreamingMethodGPU.cuh"
 #include "hoomd/GPUPolymorph.cuh"
 #include "MPCDSineGeometry.h"
@@ -11,4 +19,3 @@
 //! Template instantiation of sine geometry streaming
 template cudaError_t mpcd::gpu::confined_stream<azplugins::detail::SineGeometry>
     (const mpcd::gpu::stream_args_t& args, const azplugins::detail::SineGeometry& geom);
-
