648fddf06f0df647c6580649b7de0bdf9e3aa24e
Michael Howard <mphoward@utexas.edu>
2020-03-02 15:35:41 -0500

    Generalized wall restraints (#24)
    * Refactor plane restraint as template class
    
    * Add cylinder API and tests
    
    * Add missing cylinder and spher GPU exports
    
    * Add sphere API and tests
    
    * Cleanup a few doxygen comments
    
    * Relax decimal tolerance in tests
    
    * Only use loose comparisons in one refactored cylinder test
    
    * Fix typo in comment
    
    * Add module level documentation about restraints

---
+++ azplugins/WallRestraintComputeGPU.h
@@ -0,0 +1,104 @@
+// Copyright (c) 2018-2019, Michael P. Howard
+// This file is part of the azplugins project, released under the Modified BSD License.
+
+// Maintainer: mphoward
+
+/*! \file WallRestraintComputeGPU.h
+ *  \brief Computes harmonic restraint forces relative to a plane, on the GPU
+ */
+
+#ifndef AZPLUGINS_WALL_RESTRAINT_COMPUTE_GPU_H_
+#define AZPLUGINS_WALL_RESTRAINT_COMPUTE_GPU_H_
+
+#ifdef NVCC
+#error This header cannot be compiled by nvcc
+#endif
+
+#include "WallRestraintCompute.h"
+#include "WallRestraintComputeGPU.cuh"
+
+#include "hoomd/Autotuner.h"
+#include <hoomd/extern/pybind/include/pybind11/pybind11.h>
+
+namespace azplugins
+{
+//! Applies a harmonic force relative to a WallData object for a group of particles on the GPU
+template<class T>
+class PYBIND11_EXPORT WallRestraintComputeGPU : public WallRestraintCompute<T>
+    {
+    public:
+        //! Constructs the compute
+        /*!
+         * \param sysdef HOOMD system definition.
+         * \param group Particle group to compute on.
+         * \param wall Restraint wall.
+         * \param k Force constant.
+         */
+        WallRestraintComputeGPU(std::shared_ptr<SystemDefinition> sysdef,
+                                std::shared_ptr<ParticleGroup> group,
+                                std::shared_ptr<T> wall,
+                                Scalar k)
+            : WallRestraintCompute<T>(sysdef, group, wall, k)
+            {
+            this->m_tuner.reset(new Autotuner(32, 1024, 32, 5, 100000, "harmonic_plane", this->m_exec_conf));
+            }
+
+    protected:
+        //! Actually compute the forces on the GPU
+        /*!
+         * \param timestep Current timestep
+         *
+         * Harmonic forces are computed on all particles in group based on their distance from the surface.
+         */
+        virtual void computeForces(unsigned int timestep)
+            {
+            ArrayHandle<unsigned int> d_group(this->m_group->getIndexArray(), access_location::device, access_mode::read);
+            ArrayHandle<Scalar4> d_pos(this->m_pdata->getPositions(), access_location::device, access_mode::read);
+            ArrayHandle<int3> d_image(this->m_pdata->getImages(), access_location::device, access_mode::read);
+
+            ArrayHandle<Scalar4> d_force(this->m_force, access_location::device, access_mode::overwrite);
+            ArrayHandle<Scalar> d_virial(this->m_virial, access_location::device, access_mode::overwrite);
+
+            // zero the forces and virial before calling
+            cudaMemset((void*)d_force.data, 0, sizeof(Scalar4)*this->m_force.getNumElements());
+            cudaMemset((void*)d_virial.data, 0, sizeof(Scalar)*this->m_virial.getNumElements());
+
+            this->m_tuner->begin();
+            gpu::compute_wall_restraint(d_force.data,
+                                        d_virial.data,
+                                        d_group.data,
+                                        d_pos.data,
+                                        d_image.data,
+                                        this->m_pdata->getGlobalBox(),
+                                        *(this->m_wall),
+                                        this->m_k,
+                                        this->m_group->getNumMembers(),
+                                        this->m_virial_pitch,
+                                        this->m_tuner->getParam());
+            if (this->m_exec_conf->isCUDAErrorCheckingEnabled()) CHECK_CUDA_ERROR();
+            this->m_tuner->end();
+            }
+
+    private:
+        std::shared_ptr<Autotuner> m_tuner; //!< Tuner for force kernel
+    };
+
+namespace detail
+{
+//! Exports the WallRestraintComputeGPU to python
+/*!
+ * \param m Python module to export to.
+ * \param name Name for the potential.
+ */
+template<class T>
+void export_WallRestraintComputeGPU(pybind11::module& m, const std::string& name)
+    {
+    namespace py = pybind11;
+    py::class_<WallRestraintComputeGPU<T>,std::shared_ptr<WallRestraintComputeGPU<T>>>(m,name.c_str(),py::base<WallRestraintCompute<T>>())
+    .def(py::init<std::shared_ptr<SystemDefinition>,std::shared_ptr<ParticleGroup>,std::shared_ptr<T>,Scalar>())
+    ;
+    }
+} // end namespace detail
+} // end namespace azplugins
+
+#endif // AZPLUGINS_WALL_RESTRAINT_COMPUTE_GPU_H_
---
+++ azplugins/test-py/test_restrain_cylinder.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2018-2019, Michael P. Howard
+# This file is part of the azplugins project, released under the Modified BSD License.
+
+# Maintainer: mphoward
+
+import numpy as np
+
+import hoomd
+hoomd.context.initialize()
+from hoomd import md
+try:
+    from hoomd import azplugins
+except ImportError:
+    import azplugins
+import unittest
+
+# azplugins.restrain.cylinder
+class restrain_cylinder_tests(unittest.TestCase):
+    """Tests azplugins.restrain.cylinder"""
+
+    def setUp(self):
+        snap = hoomd.data.make_snapshot(N=3, box=hoomd.data.boxdim(L=7.0))
+        if hoomd.comm.get_rank() == 0:
+            snap.particles.position[0] = [0,0,-1]
+            snap.particles.position[1] = [1,0,0]
+            snap.particles.position[2] = [3,0,2]
+            snap.particles.image[2] = [-1,0,-1]
+        hoomd.init.read_snapshot(snap)
+
+        # dummy integrator
+        all_ = hoomd.group.all()
+        md.integrate.mode_standard(dt=0)
+        md.integrate.nve(group=all_)
+
+    def test_create(self):
+        """Test object creation and updating."""
+        f = azplugins.restrain.cylinder(group=hoomd.group.all(), radius=2, origin=(0,0,0), axis=(0,0,1), k=2.0)
+
+        f.set_params(k=5.0)
+        f.set_params(k=8)
+
+        f.set_params(radius=3.)
+        f.set_params(radius=4)
+
+        f.set_params(origin=(0,0,1))
+        f.set_params(origin=[0,0,1])
+        f.set_params(origin=np.array([0,0,1]))
+
+        f.set_params(axis=(0,0,1))
+        f.set_params(axis=[0,0,1])
+        f.set_params(axis=np.array([0,0,1]))
+
+        f.set_params(radius=2., origin=(0,0,0), axis=(1,0,0), k=10.0)
+
+    def test_force(self):
+        """Test forces computed on particles."""
+        group = hoomd.group.all()
+
+        # compute forces
+        f = azplugins.restrain.cylinder(group=group, radius=1, origin=(2,0,1), axis=(0,0,1), k=2.0)
+        hoomd.run(1)
+        np.testing.assert_array_almost_equal(f.forces[0].force, (2,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].force, (10,0,0))
+        self.assertAlmostEqual(f.forces[0].energy, 1.)
+        self.assertAlmostEqual(f.forces[1].energy, 0)
+        self.assertAlmostEqual(f.forces[2].energy, 25.)
+        np.testing.assert_array_almost_equal(f.forces[0].virial, (0,0,-2,0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].virial, (0,0,0,0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].virial, (30,0,20,0,0,0))
+
+        # change the spring constant
+        f.set_params(k=1.0)
+        hoomd.run(1)
+        np.testing.assert_array_almost_equal(f.forces[0].force, (1,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].force, (5,0,0))
+        self.assertAlmostEqual(f.forces[0].energy, 0.5)
+        self.assertAlmostEqual(f.forces[1].energy, 0)
+        self.assertAlmostEqual(f.forces[2].energy, 12.5)
+
+        # change the radius
+        f.set_params(k=2.0, radius=2)
+        hoomd.run(1)
+        np.testing.assert_array_almost_equal(f.forces[0].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].force, (-2,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].force, (8,0,0))
+        self.assertAlmostEqual(f.forces[0].energy, 0)
+        self.assertAlmostEqual(f.forces[1].energy, 1.)
+        self.assertAlmostEqual(f.forces[2].energy, 16.)
+        np.testing.assert_array_almost_equal(f.forces[0].virial, (0,0,0,0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].virial, (-2,0,0,0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].virial, (24,0,16,0,0,0))
+
+        # shift the cylinder
+        f.set_params(radius=1, origin=(1,0,0))
+        hoomd.run(1)
+        np.testing.assert_array_almost_equal(f.forces[0].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].force, (8,0,0))
+        self.assertAlmostEqual(f.forces[0].energy, 0)
+        self.assertAlmostEqual(f.forces[1].energy, 0)
+        self.assertAlmostEqual(f.forces[2].energy, 16.)
+
+        # rotate the axis
+        # test with loose tolerance because single precision has imprecise quaternion rotation
+        f.set_params(radius=1, origin=(2,0,1), axis=(1,0,0))
+        hoomd.run(1)
+        np.testing.assert_array_almost_equal(f.forces[0].force, (0,0,2), decimal=3)
+        np.testing.assert_array_almost_equal(f.forces[1].force, (0,0,0), decimal=3)
+        np.testing.assert_array_almost_equal(f.forces[2].force, (0,0,10), decimal=3)
+        self.assertAlmostEqual(f.forces[0].energy, 1., places=3)
+        self.assertAlmostEqual(f.forces[1].energy, 0, places=3)
+        self.assertAlmostEqual(f.forces[2].energy, 25., places=3)
+        np.testing.assert_array_almost_equal(f.forces[0].virial, (0,0,0,0,0,-2), decimal=3)
+        np.testing.assert_array_almost_equal(f.forces[1].virial, (0,0,0,0,0,0), decimal=3)
+        np.testing.assert_array_almost_equal(f.forces[2].virial, (0,0,0,0,0,20), decimal=3)
+
+    def test_group(self):
+        """Test forces on subgroup of prticles."""
+        # leave out particle 0
+        group = hoomd.group.tags(1,2)
+
+        # compute forces
+        f = azplugins.restrain.cylinder(group=group, radius=1, origin=(2,0,1), axis=(0,0,1), k=2.0)
+        hoomd.run(1)
+        np.testing.assert_array_almost_equal(f.forces[0].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].force, (0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].force, (10,0,0))
+        self.assertAlmostEqual(f.forces[0].energy, 0)
+        self.assertAlmostEqual(f.forces[1].energy, 0)
+        self.assertAlmostEqual(f.forces[2].energy, 25.)
+        np.testing.assert_array_almost_equal(f.forces[0].virial, (0,0,0,0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[1].virial, (0,0,0,0,0,0))
+        np.testing.assert_array_almost_equal(f.forces[2].virial, (30,0,20,0,0,0))
+
+    def tearDown(self):
+        hoomd.context.initialize()
+
+if __name__ == '__main__':
+    unittest.main(argv = ['test.py', '-v'])
