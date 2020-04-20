da5e9e49d15cc0b011b40d5d1ea05aae65353b92
Michael Howard <mphoward@utexas.edu>
2020-04-19 19:32:03 -0500

    Fix format error

--- azplugins/_azplugins.py
+++ azplugins/_azplugins.py
@@ -1,1 +1,6 @@
-"""This file is for mock imports during doc generation."""
+# Copyright (c) 2018-2020, Michael P. Howard
+# This file is part of the azplugins project, released under the Modified BSD License.
+
+# Maintainer: mphoward
+
+"""This file is only for mock imports during doc generation."""
