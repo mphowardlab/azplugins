ab888f2d5ba14f8b6345138ca56aec0be1c687b1
Arjun Goswami <66393434+arjungoswami@users.noreply.github.com>
2020-08-08 18:29:27 -0500

    Added driver file for SLLOD-Couette integrator and added to CMakeLists.txt

---
+++ azplugins/TwoStepSLLODCouette.cc
@@ -0,0 +1,11 @@
+// Copyright (c) 2018-2020, Michael P. Howard
+// This file is part of the azplugins project, released under the Modified BSD License.
+
+// Maintainer: arjunsg2
+
+/*!
+ * \file TwoStepSLLODCouette.h
+ * \brief Declaration of TwoStepSLLODCouette
+ */
+
+ #include "TwoStepSLLODCouette.h"
