b38cd13a2b9832b304248518d733252d95b76882
Arjun Goswami <66393434+arjungoswami@users.noreply.github.com>
2020-09-22 02:01:34 -0500

    Andersen bin-based profile-unbiased thermostat

--- azplugins/TwoStepSLLODCouette.h
+++ azplugins/TwoStepSLLODCouette.h
@@ -19,6 +19,7 @@
 #include "hoomd/RandomNumbers.h"
 #include "hoomd/extern/pybind/include/pybind11/pybind11.h"
 #include "RNGIdentifiers.h"
+#include <vector>
 
 namespace azplugins
 {
@@ -194,10 +195,10 @@ void TwoStepSLLODCouette::integrateStepOne(unsigned int timestep)
         // if particle leaves from (+/-) y boundary it gets (-/+) shear_rate
         // note carefully that pair potentials dependent on dv (e.g. DPD)
         // not yet explicitly supported due to minimum image convention
-        if (pos.y > y.y/2 + yz*pos.z){
+        if (pos.y > y.y/2){
             vel.x -= boundary_shear;
         }
-        if (pos.y < -y.y/2 + yz*pos.z){
+        if (pos.y < -y.y/2){
             vel.x += boundary_shear;
         }
 
@@ -229,7 +230,102 @@ void TwoStepSLLODCouette::integrateStepTwo(unsigned int timestep)
     ArrayHandle<Scalar> h_gamma(m_gamma, access_location::host, access_mode::read);
     ArrayHandle<unsigned int> h_tag(m_pdata->getTags(), access_location::host, access_mode::read);
 
+    Scalar3 binLabel[group_size];
     const Scalar currentTemp = m_T->getValue(timestep);
+    int period = 500;
+
+    // assigning particles to bins
+    BoxDim currentBox = m_pdata->getGlobalBox();
+    Scalar3 x = currentBox.getLatticeVector(0);
+    Scalar3 y = currentBox.getLatticeVector(1);
+    Scalar3 z = currentBox.getLatticeVector(2);
+    Scalar xy = currentBox.getTiltFactorXY();
+
+    // number of bins in each direction
+    int numBinsX = 10;
+    int numBinsY = 10;
+    int numBinsZ = 10;
+
+    // end positions of the bins in each direction
+    Scalar binPosX[numBinsX];
+    Scalar binPosY[numBinsY];
+    Scalar binPosZ[numBinsZ];
+
+    for (int binIdxX = 0; binIdxX < numBinsX; binIdxX++)
+    {
+        binPosX[binIdxX] = -Scalar(0.5) * x.x + (binIdxX + 1) * x.x / numBinsX;
+    }
+
+    for (int binIdxY = 0; binIdxY < numBinsY; binIdxY++)
+    {
+        binPosY[binIdxY] = -Scalar(0.5) * y.y + (binIdxY + 1) * y.y / numBinsY;
+    }
+
+    for (int binIdxZ = 0; binIdxZ < numBinsZ; binIdxZ++)
+    {
+        binPosZ[binIdxZ] = -Scalar(0.5) * z.z + (binIdxZ + 1) * z.z / numBinsX;
+    }
+
+    std::vector<Scalar> vBinPosX(binPosX, binPosX + numBinsX);
+    std::vector<Scalar> vBinPosY(binPosY, binPosY + numBinsY);
+    std::vector<Scalar> vBinPosZ(binPosZ, binPosZ + numBinsZ);
+
+    // Finding which bin each particle belongs to
+    if (timestep % period == 0){
+        for (unsigned int group_idx = 0; group_idx < group_size; group_idx++)
+        {
+            unsigned int idx = m_group->getMemberIndex(group_idx);
+
+            const Scalar4 postype = h_pos.data[idx];
+            Scalar3 pos = make_scalar3(postype.x, postype.y, postype.z);
+
+            std::vector<Scalar>::iterator itX, itY, itZ;
+
+            itX = std::lower_bound(vBinPosX.begin(), vBinPosX.end(), pos.x - xy * pos.y);
+            binLabel[idx].x = Scalar(int(itX - vBinPosX.begin()));
+
+            itY = std::lower_bound(vBinPosY.begin(), vBinPosY.end(), pos.y);
+            binLabel[idx].y = Scalar(int(itY - vBinPosY.begin()));
+
+            itZ = std::lower_bound(vBinPosZ.begin(), vBinPosZ.end(), pos.z);
+            binLabel[idx].z = Scalar(int(itZ - vBinPosZ.begin()));
+        }
+    }
+
+    // computing the bin center-of-mass velocities
+    Scalar3 Bin_COM_Vel[numBinsX][numBinsY][numBinsZ];
+
+    if (timestep % period == 0){
+        for (int binIdxX = 0; binIdxX < numBinsX; binIdxX++)
+        {
+            for (int binIdxY = 0; binIdxY < numBinsY; binIdxY++)
+            {
+                for (int binIdxZ = 0; binIdxZ < numBinsZ; binIdxZ++)
+                {
+                    Scalar3 v_sum = make_scalar3(0.0, 0.0, 0.0);
+                    int num = 0;
+                    for (unsigned int group_idx = 0; group_idx < group_size; group_idx++)
+                    {
+                        unsigned int idx = m_group->getMemberIndex(group_idx);
+                        if (   binLabel[idx].x == Scalar(binIdxX)
+                            && binLabel[idx].y == Scalar(binIdxY)
+                            && binLabel[idx].z == Scalar(binIdxZ) ){
+                                Scalar4 velmass_idx = h_vel.data[idx];
+                                Scalar3 vel = make_scalar3(velmass_idx.x, velmass_idx.y, velmass_idx.z);
+                                v_sum += vel;
+                                num += 1;
+                            }
+                    }
+                    if (num != 0){
+                        Bin_COM_Vel[binIdxX][binIdxY][binIdxZ] = v_sum / Scalar(num);
+                    }
+                    else {
+                        Bin_COM_Vel[binIdxX][binIdxY][binIdxZ] = v_sum;
+                    }
+                }
+            }
+        }
+    }
 
     // v(t+deltaT) = v(t+deltaT/2) + 1/2 * deltaT * (a(t+deltaT) - v(t+deltaT/2)*del_u)
     for (unsigned int group_idx = 0; group_idx < group_size; group_idx++)
@@ -237,37 +333,64 @@ void TwoStepSLLODCouette::integrateStepTwo(unsigned int timestep)
         unsigned int j = m_group->getMemberIndex(group_idx);
         unsigned int ptag = h_tag.data[j];
 
-        Scalar gamma;
-        if (m_use_lambda)
-            gamma = m_lambda*h_diameter.data[j];
-        else
-            {
-            unsigned int type = __scalar_as_int(h_pos.data[j].w);
-            gamma = h_gamma.data[type];
-            }
+        // Scalar gamma;
+        // if (m_use_lambda)
+        //     gamma = m_lambda*h_diameter.data[j];
+        // else
+        //     {
+        //     unsigned int type = __scalar_as_int(h_pos.data[j].w);
+        //     gamma = h_gamma.data[type];
+        //     }
+        //
+        // hoomd::RandomGenerator rng(azplugins::RNGIdentifier::TwoStepLangevinFlow, m_seed, ptag, timestep);
+        // hoomd::UniformDistribution<Scalar> uniform(Scalar(-1), Scalar(1));
+        // Scalar rx = uniform(rng);
+        // Scalar ry = uniform(rng);
+        // Scalar rz = uniform(rng);
+        // Scalar coeff = fast::sqrt(Scalar(6.0)*gamma*currentTemp/m_deltaT);
+        // if (m_noiseless)
+        //     coeff = Scalar(0.0);
+        // Scalar bd_fx = rx*coeff - gamma*(h_vel.data[j].x - h_pos.data[j].y * m_gamma_dot);
+        // Scalar bd_fy = ry*coeff - gamma*h_vel.data[j].y;
+        // Scalar bd_fz = rz*coeff - gamma*h_vel.data[j].z;
+        //
+        // // first, calculate acceleration from the net force
+        // Scalar minv = Scalar(1.0) / h_vel.data[j].w;
+        // h_accel.data[j].x = (h_net_force.data[j].x + bd_fx)*minv;
+        // h_accel.data[j].y = (h_net_force.data[j].y + bd_fy)*minv;
+        // h_accel.data[j].z = (h_net_force.data[j].z + bd_fz)*minv;
 
-        hoomd::RandomGenerator rng(azplugins::RNGIdentifier::TwoStepLangevinFlow, m_seed, ptag, timestep);
-        hoomd::UniformDistribution<Scalar> uniform(Scalar(-1), Scalar(1));
-        Scalar rx = uniform(rng);
-        Scalar ry = uniform(rng);
-        Scalar rz = uniform(rng);
-        Scalar coeff = fast::sqrt(Scalar(6.0)*gamma*currentTemp/m_deltaT);
-        if (m_noiseless)
-            coeff = Scalar(0.0);
-        Scalar bd_fx = rx*coeff - gamma*(h_vel.data[j].x - h_pos.data[j].y * m_gamma_dot);
-        Scalar bd_fy = ry*coeff - gamma*h_vel.data[j].y;
-        Scalar bd_fz = rz*coeff - gamma*h_vel.data[j].z;
+        if (timestep % period == 0) {
 
-        // first, calculate acceleration from the net force
-        Scalar minv = Scalar(1.0) / h_vel.data[j].w;
-        h_accel.data[j].x = (h_net_force.data[j].x + bd_fx)*minv;
-        h_accel.data[j].y = (h_net_force.data[j].y + bd_fy)*minv;
-        h_accel.data[j].z = (h_net_force.data[j].z + bd_fz)*minv;
+            // randomly draw from Maxwell-Boltzmann-style distribution of
+            // velocities at given temperature, with mean 0
 
-        // then, update the velocity
-        h_vel.data[j].x += Scalar(1.0/2.0)*(h_accel.data[j].x - h_vel.data[j].y * m_gamma_dot)*m_deltaT;
-        h_vel.data[j].y += Scalar(1.0/2.0)*h_accel.data[j].y*m_deltaT;
-        h_vel.data[j].z += Scalar(1.0/2.0)*h_accel.data[j].z*m_deltaT;
+            hoomd::RandomGenerator rng(azplugins::RNGIdentifier::TwoStepLangevinFlow, m_seed, ptag, timestep);
+            hoomd::NormalDistribution<Scalar> gen(fast::sqrt(currentTemp / h_vel.data[j].w), 0.0);
+            Scalar3 vel;
+            gen(vel.x, vel.y, rng);
+            vel.z = gen(rng);
+
+            // Update velocity of particle to this randomly drawn velocity
+            // plus bin center-of-mass velocity
+
+            Scalar3 v_com = Bin_COM_Vel[int(binLabel[j].x)][int(binLabel[j].y)][int(binLabel[j].z)];
+            h_vel.data[j].x = vel.x + v_com.x;
+            h_vel.data[j].y = vel.y + v_com.y;
+            h_vel.data[j].z = vel.z + v_com.z;
+        }
+        else {
+            // first, calculate acceleration from the net force
+            Scalar minv = Scalar(1.0) / h_vel.data[j].w;
+            h_accel.data[j].x = h_net_force.data[j].x * minv;
+            h_accel.data[j].y = h_net_force.data[j].y * minv;
+            h_accel.data[j].z = h_net_force.data[j].z * minv;
+
+            // then, update the velocity
+            h_vel.data[j].x += Scalar(1.0/2.0)*(h_accel.data[j].x - h_vel.data[j].y * m_gamma_dot)*m_deltaT;
+            h_vel.data[j].y += Scalar(1.0/2.0)*h_accel.data[j].y*m_deltaT;
+            h_vel.data[j].z += Scalar(1.0/2.0)*h_accel.data[j].z*m_deltaT;
+        }
         }
 
     if (m_prof) m_prof->pop();
