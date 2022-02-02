c300799cfcdfd1841f6ceca7abba9099f9fefd24
Michael Howard <mphoward@auburn.edu>
2022-02-01 21:44:18 -0600

    Update copyright to 2022

--- azplugins/BondEvaluatorFENE.h
+++ azplugins/BondEvaluatorFENE.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2018-2020, Michael P. Howard
-// Copyright (c) 2021, Auburn University
+// Copyright (c) 2021-2022, Auburn University
 // This file is part of the azplugins project, released under the Modified BSD License.
 
 /*! \file BondEvaluatorFENE.h
