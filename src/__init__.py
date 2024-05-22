8d081f1eb14f523053423d3c3f7420ed61589274
Michael Howard <mphoward@auburn.edu>
2024-05-22 15:00:36 -0500

    Refactor to use new component build system

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2018-2020, Michael P. Howard
+# Copyright (c) 2021-2022, Auburn University
+# This file is part of the azplugins project, released under the Modified BSD License.
+
+__version__ = '1.0.0'
