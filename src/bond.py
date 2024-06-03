e2c1c5feb56a8563a7e924516e9581e89b057bc1
Levi Petix <clpetix@gmail.com>
2024-06-03 15:58:46 -0500

    Update DoubleWell bond documentation.

--- src/bond.py
+++ src/bond.py
@@ -15,7 +15,43 @@ class _Bond(bond.Bond):
 
 
 class DoubleWell(_Bond):
-    r"""Double-well bond potential."""
+    r"""Double-well bond potential.
+
+    `DoubleWell` specifies a double well potential between the two particles in
+    the simulation state with:
+
+    .. math::
+
+        U(r)  =  \frac{V_{max}-c/2}{b^4} \left[
+            \left( r - a/2 \right)^2 - b^2 \right]^2
+            +\frac{c}{2b}(r - a/2) + c/2
+
+    Attributes:
+        params (TypeParameter[``bond type``, dict]):
+            The parameter of the double-well bonds for each particle type.
+            The dictionary has the following keys:
+
+            * ``V_max`` (`float`, **required**) - Potential maximum energy
+                barrier between the two minima at ``a/2`` for c=0 (in energy
+                units)
+
+            * ``a`` (`float`, **required**) - twice the location of the
+                potential maximum, maximum is at ``a/2`` for c=0 ( in distance
+                units)
+
+            * ``b`` (`float`, **required**) - tunes the distance between the
+                potential minima at ``(a/2 +/- b)`` for c=0 (in distance units)
+
+            * ``c`` (`float`, **required**) -tunes the energy offset between the
+                two potential minima values, i.e. it tilts the potential (in
+                energy units). The default value of c is zero.
+
+    Examples::
+
+        dw = azplugins.bond.DoubleWell()
+        dw.params['A-A'] = dict(V_max=2.0, a=2.5, b=0.5)
+        dw.params['A-A'] = dict(V_max=2.0, a=2.5, b=0.2, c=1.0)
+    """
 
     _cpp_class_name = 'PotentialBondDoubleWell'
 
