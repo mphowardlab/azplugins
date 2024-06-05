9b3aed148b4986ac275171dcf35a8010c384ae71
Michael Howard <mphoward@auburn.edu>
2024-06-05 14:06:02 -0400

    Refactor double well bond potential (#75)
    * Refactor double well bond potential
    
    * Update DoubleWell bond documentation.
    
    * Refactor parameters to r_0, r_1, U_1, and U_tilt.
    
    * Update unit test names.
    
    * Fix implementation of new potential form.
    
    * Update unit tests to be sensible.
    
    * Update documentation.
    
    * Fix typo in documentation.
    
    * Apply final changes from code review
    
    ---------
    
    Co-authored-by: Levi Petix <clpetix@gmail.com>

--- src/module.cc
+++ src/module.cc
@@ -50,10 +50,14 @@ namespace azplugins
 namespace detail
     {
 
+void export_PotentialBondDoubleWell(pybind11::module&);
+
 void export_PotentialPairHertz(pybind11::module&);
 void export_PotentialPairPerturbedLennardJones(pybind11::module&);
 
 #ifdef ENABLE_HIP
+void export_PotentialBondDoubleWellGPU(pybind11::module&);
+
 void export_PotentialPairHertzGPU(pybind11::module&);
 void export_PotentialPairPerturbedLennardJonesGPU(pybind11::module&);
 #endif // ENABLE_HIP
@@ -67,10 +71,14 @@ PYBIND11_MODULE(_azplugins, m)
     {
     using namespace hoomd::azplugins::detail;
 
+    export_PotentialBondDoubleWell(m);
+
     export_PotentialPairHertz(m);
     export_PotentialPairPerturbedLennardJones(m);
 
 #ifdef ENABLE_HIP
+    export_PotentialBondDoubleWellGPU(m);
+
     export_PotentialPairHertzGPU(m);
     export_PotentialPairPerturbedLennardJonesGPU(m);
 #endif // ENABLE_HIP
