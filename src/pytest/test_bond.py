ba72266a47d51b6ba75d43da8b2b28d9cf4925a9
bcjeong1616 <bcjeong1616@gmail.com>
2024-07-18 17:40:01 -0500

    Trimmed the list of quartic bond tests. Removed the "without delta passed" case due to incompatibility

--- src/pytest/test_bond.py
+++ src/pytest/test_bond.py
@@ -73,23 +73,7 @@ potential_tests += [
     # test potential with sigma = epsilon = 0
     PotentialTestCase(
         hoomd.azplugins.bond.Quartic,
-        dict(epsilon=0.0,sigma=0.0,k=1434.3,r_0=1.5,b_1=-0.7589,b_2=0,U_0=67.2234,delta=0.0),
-        1,
-        20.80586625,
-        -99.2177025,
-    ),
-    # test potential with sigma != 0, epsilon = 0
-    PotentialTestCase(
-        hoomd.azplugins.bond.Quartic,
-        dict(epsilon=0.0,sigma=1.0,k=1434.3,r_0=1.5,b_1=-0.7589,b_2=0,U_0=67.2234,delta=0),
-        1,
-        20.80586625,
-        -99.2177025,
-    ),
-    # test potential with sigma == 0, epsilon != 0
-    PotentialTestCase(
-        hoomd.azplugins.bond.Quartic,
-        dict(epsilon=1.0,sigma=0.0,k=1434.3,r_0=1.5,b_1=-0.7589,b_2=0,U_0=67.2234,delta=0),
+        dict(k=1434.3,r_0=1.5,b_1=-0.7589,b_2=0,U_0=67.2234,sigma=0.0,epsilon=0.0,delta=0.0),
         1,
         20.80586625,
         -99.2177025,
@@ -106,14 +90,6 @@ potential_tests += [
     PotentialTestCase(
         hoomd.azplugins.bond.Quartic,
         dict(epsilon=1.0, sigma=1.0, k=1434.3,r_0=1.5,b_1=-0.7589, b_2=0,U_0=67.2234,delta=0.0),
-        1,
-        21.80586625,
-        -75.2177025,
-    ),
-    # test potential without delta passed
-    PotentialTestCase(
-        hoomd.azplugins.bond.Quartic,
-        dict(epsilon=1.0, sigma=1.0, k=1434.3,r_0=1.5,b_1=-0.7589, b_2=0,U_0=67.2234),
         1,
         21.80586625,
         -75.2177025,
