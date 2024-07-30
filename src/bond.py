a7cd1ee6cad85698776d3a3b2a619cf5a3c64691
bcjeong1616 <bcjeong1616@gmail.com>
2024-07-30 12:08:48 -0500

    added whitespace for nowrap option

--- src/bond.py
+++ src/bond.py
@@ -73,6 +73,7 @@ class Quartic(bond.Bond):
 
     .. math::
         :nowrap:
+
         \begin{eqnarray*}
         U(r) &= k (r - \Delta - r_0 - b_1)(r - \Delta - r_0 - b_2)(r - \Delta - r_0)^2
                   + U_0 + U_{\rm WCA}(r), & r < r_0 + \Delta \\
