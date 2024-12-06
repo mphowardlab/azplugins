5a91c706ff7f325075f85ff38c62be93bc569ce2
Michael Howard <mphoward@auburn.edu>
2024-12-06 14:35:41 -0600

    Support HOOMD 5 build configuration

--- doc/conf.py
+++ doc/conf.py
@@ -14,79 +14,79 @@
 import datetime
 import os
 
-project = 'azplugins'
+project = "azplugins"
 year = datetime.date.today().year
-copyright = f'2018-2020, Michael P. Howard. 2021-{year}, Auburn University.'  # noqa: A001
-author = 'Michael P. Howard'
-release = '1.0.0'
+copyright = f"2018-2020, Michael P. Howard. 2021-{year}, Auburn University."
+author = "Michael P. Howard"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-default_role = 'any'
+default_role = "any"
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 extensions = [
-    'sphinx.ext.mathjax',
+    "sphinx.ext.mathjax",
 ]
 
-extensions += ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions += ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 autodoc_docstring_signature = True
-autodoc_typehints_format = 'short'
-autodoc_mock_imports = ['hoomd.azplugins._azplugins']
-if os.getenv('READTHEDOCS'):
+autodoc_typehints_format = "short"
+autodoc_mock_imports = ["hoomd.azplugins._azplugins"]
+if os.getenv("READTHEDOCS"):
     autodoc_mock_imports += [
-        'hoomd._hoomd',
-        'hoomd.version_config',
-        'hoomd.md._md',
-        'hoomd.metal._metal',
-        'hoomd.mpcd._mpcd',
-        'hoomd.minimize._minimize',
-        'hoomd.hpmc._jit',
-        'hoomd.hpmc._hpmc',
+        "hoomd._hoomd",
+        "hoomd.version_config",
+        "hoomd.md._md",
+        "hoomd.metal._metal",
+        "hoomd.mpcd._mpcd",
+        "hoomd.minimize._minimize",
+        "hoomd.hpmc._jit",
+        "hoomd.hpmc._hpmc",
     ]
 
-extensions += ['sphinx.ext.napoleon']
+extensions += ["sphinx.ext.napoleon"]
 napoleon_include_special_with_doc = True
 
-extensions += ['sphinx.ext.intersphinx']
+extensions += ["sphinx.ext.intersphinx"]
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://numpy.org/doc/stable', None),
-    'gsd': ('https://gsd.readthedocs.io/en/stable/', None),
-    'hoomd': ('https://hoomd-blue.readthedocs.io/en/stable/', None),
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "gsd": ("https://gsd.readthedocs.io/en/stable/", None),
+    "hoomd": ("https://hoomd-blue.readthedocs.io/en/stable/", None),
 }
 
-extensions += ['sphinx.ext.todo']
+extensions += ["sphinx.ext.todo"]
 todo_include_todos = False
 
-extensions += ['sphinx_copybutton']
-copybutton_prompt_text = '$ '
+extensions += ["sphinx_copybutton"]
+copybutton_prompt_text = "$ "
 copybutton_remove_prompts = True
-copybutton_line_continuation_character = '\\'
+copybutton_line_continuation_character = "\\"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 # html_logo = "hoomdblue-logo-vertical.svg"
 html_theme_options = {
     # "sidebar_hide_name": True,
-    'top_of_page_buttons': [],
-    'navigation_with_keys': True,
-    'dark_css_variables': {
-        'color-brand-primary': '#5187b2',
-        'color-brand-content': '#5187b2',
+    "top_of_page_buttons": [],
+    "navigation_with_keys": True,
+    "dark_css_variables": {
+        "color-brand-primary": "#5187b2",
+        "color-brand-content": "#5187b2",
     },
-    'light_css_variables': {
-        'color-brand-primary': '#406a8c',
-        'color-brand-content': '#406a8c',
+    "light_css_variables": {
+        "color-brand-primary": "#406a8c",
+        "color-brand-content": "#406a8c",
     },
 }
 # html_favicon = "hoomdblue-logo-favicon.svg"
 
-pygments_style = 'friendly'
-pygments_dark_style = 'native'
+pygments_style = "friendly"
+pygments_dark_style = "native"
