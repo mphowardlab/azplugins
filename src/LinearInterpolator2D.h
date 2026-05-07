c019b9f8757b5a1c441bfb458a949562e991d0c2
mayukh33 <mzk0148@auburn.edu>
2026-05-07 18:11:59 +0200

    Fix pre-commit error

--- src/LinearInterpolator2D.h
+++ src/LinearInterpolator2D.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2020, Michael P. Howard
+// Copyright (c) 2021-2025, Auburn University
+// Part of azplugins, released under the BSD 3-Clause License.
+
 #ifndef AZPLUGINS_LINEAR_INTERPOLATOR_2D_H_
 #define AZPLUGINS_LINEAR_INTERPOLATOR_2D_H_
 
@@ -38,10 +42,7 @@ template<typename T> class LinearInterpolator2D
 
     //! Constructor
     AZPLUGINS_HOSTDEVICE AZPLUGINS_FORCEINLINE
-    LinearInterpolator2D(const T* data,
-                         const unsigned int* n,
-                         const Scalar* lo,
-                         const Scalar* hi)
+    LinearInterpolator2D(const T* data, const unsigned int* n, const Scalar* lo, const Scalar* hi)
         : m_data(data), m_indexer(n[0], n[1])
         {
         for (int d = 0; d < 2; ++d)
@@ -49,7 +50,7 @@ template<typename T> class LinearInterpolator2D
             const unsigned int nd = n[d];
             assert(nd >= 2);
 
-            m_n[d]  = nd;
+            m_n[d] = nd;
             m_lo[d] = lo[d];
             m_hi[d] = hi[d];
             m_dx[d] = (m_hi[d] - m_lo[d]) / Scalar(nd - 1);
@@ -58,9 +59,9 @@ template<typename T> class LinearInterpolator2D
 
     //! Destructor
     ~LinearInterpolator2D()
-    	{
-	delete[] m_data;
-	}
+        {
+        delete[] m_data;
+        }
 
     //! Interpolate at (x0, x1)
     AZPLUGINS_HOSTDEVICE AZPLUGINS_FORCEINLINE Scalar operator()(Scalar x0, Scalar x1) const
@@ -81,22 +82,22 @@ template<typename T> class LinearInterpolator2D
                 {
                 --b;
                 }
-            
-	    assert(b >= 0);
-	    assert(b < static_cast<int>(nd - 1));
-            bin[d]  = b;
+
+            assert(b >= 0);
+            assert(b < static_cast<int>(nd - 1));
+            bin[d] = b;
             frac[d] = f - Scalar(b);
             }
 
         const unsigned int b0 = static_cast<unsigned int>(bin[0]);
         const unsigned int b1 = static_cast<unsigned int>(bin[1]);
-        const Scalar c00 = m_data[m_indexer(b0,     b1)];
-        const Scalar c01 = m_data[m_indexer(b0,     b1 + 1)];
+        const Scalar c00 = m_data[m_indexer(b0, b1)];
+        const Scalar c01 = m_data[m_indexer(b0, b1 + 1)];
         const Scalar c10 = m_data[m_indexer(b0 + 1, b1)];
         const Scalar c11 = m_data[m_indexer(b0 + 1, b1 + 1)];
 
         // Bilinear interpolation
-        const Scalar t0   = frac[0];
+        const Scalar t0 = frac[0];
         const Scalar omt0 = Scalar(1) - t0;
         const Scalar c0 = c00 * omt0 + c10 * t0;
         const Scalar c1 = c01 * omt0 + c11 * t0;
@@ -117,12 +118,12 @@ template<typename T> class LinearInterpolator2D
         }
 
     private:
-    const T*     m_data;
-    Scalar       m_lo[2];
-    Scalar       m_hi[2];
-    Scalar       m_dx[2];
+    const T* m_data;
+    Scalar m_lo[2];
+    Scalar m_hi[2];
+    Scalar m_dx[2];
     unsigned int m_n[2];
-    Index2D      m_indexer;
+    Index2D m_indexer;
     };
 
     } // namespace azplugins
--- src/PerturbedLennardJonesEvap.h
+++ src/PerturbedLennardJonesEvap.h
@@ -6,8 +6,8 @@
 #define AZPLUGINS_PERTURBED_LENNARD_JONES_EVAP_H_
 
 #include <memory>
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 #include "hoomd/BoxDim.h"
 #include "hoomd/ForceCompute.h"
@@ -32,9 +32,9 @@ class PYBIND11_EXPORT PerturbedLennardJonesEvap : public ForceCompute
                               Scalar r_cut,
                               Scalar epsilon,
                               Scalar sigma,
-			      Scalar dt,
-			      Scalar initial_delta,
-			      Scalar solventD,
+                              Scalar dt,
+                              Scalar initial_delta,
+                              Scalar solventD,
                               const Scalar* lambda_data,
                               const unsigned int* lambda_shape,
                               const Scalar* domain);
@@ -42,34 +42,34 @@ class PYBIND11_EXPORT PerturbedLennardJonesEvap : public ForceCompute
     virtual ~PerturbedLennardJonesEvap();
 
     Scalar getRCut() const
-    	{
-	return m_r_cut;
-	}
+        {
+        return m_r_cut;
+        }
 
     Scalar getEpsilon() const
-    	{
-	return m_epsilon;
-	}
-    
+        {
+        return m_epsilon;
+        }
+
     Scalar getSigma() const
-    	{
-	return m_sigma;
-	}
+        {
+        return m_sigma;
+        }
 
     Scalar getInitial_delta() const
-    	{
-	return m_initial_delta;
-	}
+        {
+        return m_initial_delta;
+        }
 
     Scalar getSolventD() const
-    	{
-	return m_solventD;
-	}
-    
-   const GPUArray<Scalar>& getLambdaDomain() const
-	{
-	return m_domain;
-	}
+        {
+        return m_solventD;
+        }
+
+    const GPUArray<Scalar>& getLambdaDomain() const
+        {
+        return m_domain;
+        }
 
     protected:
     std::shared_ptr<hoomd::md::NeighborList> m_nlist; //!< Neighbor list
@@ -83,9 +83,9 @@ class PYBIND11_EXPORT PerturbedLennardJonesEvap : public ForceCompute
     Scalar m_initial_delta;
     Scalar m_solventD;
 
-    GPUArray<Scalar>       m_domain;        //!< Scaled coordinate domain [y_lo, y_hi, t_lo, t_hi]
-    GPUArray<Scalar>       m_lambda_data;   //!< Flattened scaled position scaled time data
-    GPUArray<unsigned int> m_lambda_shape;  //!< [ny, nt]
+    GPUArray<Scalar> m_domain;             //!< Scaled coordinate domain [y_lo, y_hi, t_lo, t_hi]
+    GPUArray<Scalar> m_lambda_data;        //!< Flattened scaled position scaled time data
+    GPUArray<unsigned int> m_lambda_shape; //!< [ny, nt]
 
     void computeForces(uint64_t timestep) override;
     };
